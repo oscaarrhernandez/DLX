5ae14e910e89817c7c0ba54ea87e6416117b9f8c
Oscar Hernandez <66411411+oscaarrhernandez@users.noreply.github.com>
2024-04-29 17:34:17 +0200

    No_Optimizado hecho

--- DLX_NoOptimizado.s
+++ DLX_NoOptimizado.s
@@ -1,7 +1,7 @@
 .data 
 ;; VARIABLES DE ENTRADA Y SALIDA: NO MODIFICAR ORDEN 
 ; VARIABLE DE ENTRADA: (SE PODRA MODIFICAR EL VALOR ENTRE 1 Y 100) 
-valor_inicial:   .word   7
+valor_inicial:   .word   10
  
 ;; VARIABLES DE SALIDA: 
 secuencia:   .space  120*4 
@@ -20,97 +20,156 @@ Printvalue:  .space 4
 .text
 .global main
 
-; r0 -> 0													 r1 -> secuencia
-; r2 -> indice de la secuencia		 r3 -> valor 3 para la multiplicacion
-; r4 -> tamaño_secuencia					 r5 -> valor inicial
-; r6 A[n-1] -> almacenamos siempre este valor en la secuencia
-; r7 A[n]
-; r8 comprobacion salida de bucle  r9 comprobacion si es impar
-; r14 -> PrintPar 								 r31 -> finish
-; r10 -> valor maximo							 r11 -> valor medio
-
 main:
-		lw r5,valor_inicial ;Cargar valor inicial
-		add r6,r0,r5 ; Pasamos a r6 el valor inicial
+		lw r4,valor_inicial
 
-		addi r1,r0,secuencia ;Cargar la primera posicion secuencia
-		sb 0(r1),r0
-		addi r1, r1, #4 ;Mover a la siguiente posición de la secuencia
-		sb 0(r1),r6 ;Guardar valor inicial en la secuencia
-		addi r1, r1, #4 ;Mover a la siguiente posición de la secuencia
+		movi2fp f1, r4
+		cvti2f f1,f1
 
-		add r10,r0,r6 ;Cargar valor maximo
+		addi r1,r0,secuencia
+		sf 0(r1),f1
+		addi r1,r1, #4
 
-    ;addi r2,r0,1 ;indice de la secuencia
-		lw r4,secuencia_tamanho ;Cargar tamaño de la secuencia
-		addi r4,r0,1 ; r4(registro de tamsecuencia) = 1
-		sw secuencia_tamanho,r4 ;Guardar tamaño de la secuencia
-		
-		add r7,r0,r6; A[n]
-		add r3,r0,3 ; valor 3 usado en x3
+		addi r2,r0,1
+
+		movi2fp f2, r2
+		cvti2f f2,f2
+
+		addi r3,r0,3
+		movi2fp f3, r3
+		cvti2f f3,f3
+
 
 loop:
-		subi r8,r6,1 ; r6-1=r8 ;;;; si A[n-1] es 1 finaliza
-		jal print ; imprimir por terminal el valor de A[n]
+		subi r8,r4,1
+		beqz r8,jump
+		
+		addi r2,r2,1
 
-		sb 0(r1),r6 ; Guardar A[n-1] en la secuencia
-		addi r1,r1,4 ; Mover a la siguiente posición de la secuencia
+		andi r9,r4,1
+		beqz r9,par
+		;impar
+		mult r10,r4,r3
+		addi r10,r10,1
+		add r4,r10,r0
 
-		beqz r8, finbucle ; r8 = A[n-1]-1 == 0 saltar a finbucle
+		movi2fp f1,r4
+		cvti2f f1,f1
 
-		addi r4,r4,1 ; suma 1 a tamsecuencia
-		sw secuencia_tamanho,r4 ;Guardar tamaño de la secuencia
+		sf 0(r1),f1
+		addi r1,r1,#4
 
-		andi r9,r6,1 ; si A[n-1] es impar
-		beqz r9,par ; r6 AND 1 == 0 saltar a par
-		mult r7,r3,r6 ; Multiplicacion Secuencia impar
-		addi r7,r7,1; Sumarle uno a la multiplicación
-		add r6,r7,r0 ; A[n-1]=A[n]
-
-		sb 0(r1),r6 ; Guardar A[n-1] en la secuencia
-		addi r1,r1,4 ; Mover a la siguiente posición de la secuencia
-
-		slt r12,r10,r6 ; si r10 < r6 r12==1 sino r12==0
-		bnez r12,savemayor; si r12==1 saltar a savemayor
 
 		j loop
 
 par:
-		srli r7,r6,1 ; A[n]=A[n-1]/2 desplazamiento logico inmediato a la derecha
-		add r6,r7,r0 ; A[n-1]=A[n]
+		srli r6,r4,1
+		add r4,r6,r0
 
-		sb 0(r1),r6 ; Guardar A[n-1] en la secuencia
-		addi r1,r1,4 ; Mover a la siguiente posición de la secuencia
-
-		addi r4,r4,1 ; suma 1 a tamsecuencia
-		sw secuencia_tamanho,r4 ;Guardar tamaño de la secuencia
-
-		slt r12,r10,r6 ; si r10 < r6 r12==1 sino r12==0
-		bnez r12,savemayor; si r12==1 saltar a savemayor
+		movi2fp f1,r4
+		cvti2f f1,f1
+		
+		sf 0(r1),f1
+		addi r1,r1,#4
 
 		j loop
 
-savemayor:
-		add r10,r6,r0 ; r10 = A[n]
-		j loop
-
-finbucle:
-		;secuencia tamaño en r4 vT 							; valor inicial en r5 vIni
-		;valor maximo en r10 vMax 							; valor medio en r11 vMed
-		
-		
 
 
-		sw secuencia_maximo,r10 ;Guardar valor maximo
-		sw secuencia_tamanho,r4 ;Guardar tamaño de la secuencia
-		sw secuencia_valor_medio,r11 ;Guardar valor medio
-		j finish;
+jump:
+		movi2fp f2, r2
+		cvti2f f2,f2
+		sf secuencia_tamanho,f2
+		addf f10,f0,f0
+		addi r1,r0,secuencia
+		lf f4,0(r1)
+		lf f11,0(r1)
+		addf f10,f10,f4
+		addi r1,r1,#4
+		subi r2,r2,1
+		beqz r2,calculos
 
-print:
-		sw Printvalue,r6
-		addi r14,r0,PrintPar
-		trap 5
-		jr r31
+		lf f5, 0(r1)
+		addf f10,f10,f5
+		addi r1,r1,#4
+
+		j loopmax
+
+loopmax:
+		subi r2,r2,1
+		beqz r2,calculos
+		gef f4,f5
+		bfpt simayor
+		bfpf simenor
+		j loopmax
+
+simayor:
+		lf f5, 0(r1)
+		addf f10,f10,f5
+		addi r1,r1,#4
+		j loopmax
+
+simenor:
+		lf f4,0(r1)
+		addf f10,f10,f4
+		addi r1,r1,#4
+		j loopmax
+
+calculos:
+		sf secuencia_maximo,f5
+		; f10 -> suma secuencia
+		; f11 -> vIni
+		; f5  -> vMax
+		; f2  -> vT
+		; f12 -> vMed
+		divf f12,f10,f2
+		sf secuencia_valor_medio,f12
+
+		addi r1,r0,lista
+		;vIni*vT
+		multf f15,f11,f2
+		sf 0(r1),f15
+		addi r1,r1,#4
+		;vMax*vT
+		multf f16,f5,f2
+		sf 0(r1),f16
+		addi r1,r1,#4
+		;vMed*vT
+		multf f17,f12,f2
+		sf 0(r1),f17
+		addi r1,r1,#4
+		;(vIni/vMax)*vT
+		divf f18,f11,f5
+		multf f19,f18,f2
+		sf 0(r1),f19
+		addi r1,r1,#4
+		;(vIni/vMed)*vT
+		divf f20,f11,f12
+		multf f21,f20,f2
+		sf 0(r1),f21
+		addi r1,r1,#4
+		;(vMax/vIni)*vT
+		divf f22,f5,f11
+		multf f23,f22,f2
+		sf 0(r1),f23
+		addi r1,r1,#4
+		;(vMax/vMed)*vT
+		divf f24,f5,f12
+		multf f25,f24,f2
+		sf 0(r1),f25
+		addi r1,r1,#4
+		;(vMed/vIni)*vT
+		divf f26,f12,f11
+		multf f27,f26,f2
+		sf 0(r1),f27
+		addi r1,r1,#4
+		;(vMed/vMax)*vT
+		divf f28,f12,f5
+		multf f29,f28,f2
+		sf 0(r1),f29
+		addi r1,r1,#4
+
+		j finish
 
 finish:
 		trap 0
